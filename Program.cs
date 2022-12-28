36b543df54cc0638ed6355b6f8ab0c986c4a52a7
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 16:01:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string sicilnumarasi;
+double calisma_saati;
+double saatlik_ücreti;
+double maas_hesabi;
+
+Console.WriteLine("Sicil Numaranızı giriniz : ");
+sicilnumarasi = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Çalışma saatinizi giriniz : ");
+calisma_saati=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Saatlik ücretinizi giriniz : ");
+saatlik_ücreti = Convert.ToDouble(Console.ReadLine());
+
+//maaş hesabı çalışma sati ile saatlik ücretinin çarpılması
+maas_hesabi =Convert.ToDouble(calisma_saati*saatlik_ücreti);
+Console.WriteLine("Alacağınız ücret : " + maas_hesabi);
+
